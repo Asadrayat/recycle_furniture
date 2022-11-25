bca7abf8e6c81b25d36157a85ba634ecf9024135
Asadrayat <asad.rayat4@gmail.com>
2022-11-25 15:45:17 +0600

    added checkout

--- src/Pages/CheckOut/CheckOut.js
+++ src/Pages/CheckOut/CheckOut.js
@@ -1,11 +1,26 @@
+import { useQuery } from '@tanstack/react-query';
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
+import Furniture from './Furniture/Furniture';
 
 const CheckOut = () => {
-    const {name} = useLoaderData();
+    const { name } = useLoaderData();
+    const { data: furnitures = [] } = useQuery({
+        queryKey: ['furnitures'],
+        queryFn: async () => {
+            const res = await fetch(`http://localhost:5000/${name}`);
+            const data = await res.json();
+            return data
+        } 
+    })
     return (
-        <div>
-            <p>{name}</p>
+        <div className='grid lg:grid-cols-2 my-12  grid-cols-1 align-middle'>
+            {
+                furnitures.map(furniture => <Furniture
+                    key={furniture._id}
+                    furniture={furniture}
+                ></Furniture>)
+            }
         </div>
     );
 };
