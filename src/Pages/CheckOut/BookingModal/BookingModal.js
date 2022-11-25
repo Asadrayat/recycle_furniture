ae878820feb04973673462221886eb8787cae892
Asadrayat <asad.rayat4@gmail.com>
2022-11-25 21:53:01 +0600

    added modal form

--- src/Pages/CheckOut/BookingModal/BookingModal.js
+++ src/Pages/CheckOut/BookingModal/BookingModal.js
@@ -1,9 +1,31 @@
 import React, { useContext } from 'react';
 import { AuthContext } from '../../../Context/Authprovider/Authprovider';
 
-const BookingModal = ({furnitureOption}) => {
-    const {title} = furnitureOption;
-    const {user} = useContext(AuthContext);
+const BookingModal = ({ furnitureOption }) => {
+    const { catagory, location, picture, used, _id, title, seller, resale_price, posted, original_price, about } = furnitureOption;
+    const { user } = useContext(AuthContext);
+    const handleBooking = event => {
+        event.preventDefault();
+        const form = event.target;
+        const resale_price = form.resale_price.value;
+        const displayName = form.displayName.value;
+        const pickup = form.pickup.value;
+        const title = form.title.value;
+        console.log(displayName);
+        const email = form.email.value;
+        const phone = form.phone.value;
+        console.log(resale_price,title,pickup, displayName, email, phone);
+
+        // const booking = {
+        //     name: title,
+        //     treatement: treatementName,
+        //     patient: displayName,
+        //     phone,
+        //     price,
+        //     email,
+        //     slot,
+        // }
+    }
     return (
         <div>
             <>
@@ -12,10 +34,10 @@ const BookingModal = ({furnitureOption}) => {
                     <div className="modal-box relative">
                         <label htmlFor="booking-modal" className="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
                         <h3 className="text-lg font-bold">Furniture</h3>
-                        <form /* onSubmit={handleBooking} */ className='grid grid-cols-1 gap-3 mt-10'>
-                            <input name="name" type="text" defaultValue={title}  disabled className="input w-full input-bordered" />
-                            <input name="name" type="text"  disabled className="input w-full input-bordered" />
-                        
+                        <form onSubmit={handleBooking} className='grid grid-cols-1 gap-3 mt-10'>
+                            <input name="title" type="text" defaultValue={title} disabled className="input w-full input-bordered" />
+                            <input name="resale_price" type="text" defaultValue={resale_price} disabled className="input w-full input-bordered" />
+
                             <input name="displayName" type="text" defaultValue={user?.displayName} placeholder="Your name" className="input w-full input-bordered" />
 
                             <input name="email" type="email" disabled defaultValue={user?.email} placeholder="Email Address" className="input w-full input-bordered" />
