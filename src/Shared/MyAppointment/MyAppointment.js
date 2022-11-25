8dba0328db4d6fb88be7f0b13c5185116a1a5c4d
Asadrayat <asad.rayat4@gmail.com>
2022-11-26 03:45:24 +0600

    Added My Appointment

--- src/Shared/MyAppointment/MyAppointment.js
+++ src/Shared/MyAppointment/MyAppointment.js
@@ -1,9 +1,67 @@
-import React from 'react';
+import { useQuery } from "@tanstack/react-query";
+import { useContext } from "react";
+import { Link } from "react-router-dom";
+import { AuthContext } from "../../Context/Authprovider/Authprovider";
+
 
 const MyAppointment = () => {
+    const { user } = useContext(AuthContext);
+    const url = `http://localhost:5000/bookings?email=${user?.email}`;
+    const { data: bookings = [] } = useQuery({
+        queryKey: ['bookings', user?.email],
+        queryFn: async () => {
+            const res = await fetch(url, {
+                // headers: {
+                //     authorization: `bearer ${localStorage.getItem('accessToken')}`
+                // }
+            });
+            const data = await res.json();
+            return data;
+        }
+    })
+    console.log(bookings);
     return (
         <div>
-            
+            <p className='text-3xl my-5'>My Appointment</p>
+            <div className="overflow-x-auto">
+                <table className="table w-full">
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Title</th>
+                            <th>Seller</th>
+                            <th>Pickup</th>
+                            <th>Price</th>
+                            <th>Payment</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            bookings?.map((booking, i) => <tr>
+                                <th>{i+1}</th>
+                                <td>{booking.title}</td>
+                                <td>{booking.seller}</td>
+                                <td>{booking.pickup}</td>
+                                <td>{booking.resale_price}</td>
+                                <td>
+                                    {
+                                        booking.price && !booking.paid && <Link
+                                            to={`/dashboard/payment/${booking._id}`}
+                                        >
+                                            <button
+                                                className='btn btn-primary btn-sm'
+                                            >Pay</button>
+                                        </Link>
+                                    }
+                                    {
+                                        booking.price && booking.paid && <span className='text-green-500'>Paid</span>
+                                    }
+                                </td>
+                            </tr>)
+                        }
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
